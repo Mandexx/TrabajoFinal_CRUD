e192c2459c03d608bb8689cc83bff265aa6429c5
Mandexx <82420316+Mandexx@users.noreply.github.com>
2021-05-01 16:06:12 -0500

    Add files via upload

---
+++ precache-manifest.5a9c873953df5e5fe19ee4b3a9ac7250.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edeb38e4cf80cfcf8dc1ffb30814b716",
+    "url": "/Mandexx/final/index.html"
+  },
+  {
+    "revision": "1a50b6d821265c9b84a5",
+    "url": "/Mandexx/final/static/css/main.d7fb5368.chunk.css"
+  },
+  {
+    "revision": "6ba5c9f774600623aa1f",
+    "url": "/Mandexx/final/static/js/2.31607cec.chunk.js"
+  },
+  {
+    "revision": "de1cedeacf4641d6277b42334cfacb1d",
+    "url": "/Mandexx/final/static/js/2.31607cec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a50b6d821265c9b84a5",
+    "url": "/Mandexx/final/static/js/main.d270a79d.chunk.js"
+  },
+  {
+    "revision": "a2a8ac040153b401932a",
+    "url": "/Mandexx/final/static/js/runtime-main.1646ed25.js"
+  }
+]);
